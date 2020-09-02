c513c047d7e45b494b0d71d0c89fa0adfafe9e69
gmaxn <maxi.ng@hotmail.com>
2020-09-01 21:14:45 -0300

    adding first logic to html *ngIf - *ngFor

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
   filterTerm: string = 'Cart';
+  products: any[] = [
+    // {
+    //   "productId": 1,
+    //   "name": "Samsung Galaxy S20 Ultra",
+    //   "code": "CEL-001",
+    //   "releaseDate": "March 18, 2019",
+    //   "price": 899.99,
+    //   "stock": 10,
+    // },
+    // {
+    //   "productId": 2,
+    //   "name": "IPhone 11 Pro",
+    //   "code": "CEL-002",
+    //   "releaseDate": "January 15, 2019",
+    //   "price": 1199.95,
+    //   "stock": 10,
+    // }
+  ]
 }
